bee7d34ee94f27fd9ebab5a804ba07244403fc5b
Tretiakova Marina <m1r2nda@skbkontur.ru>
2018-05-20 19:32:49 +0500

    Task 1. Prepare infrastructure

---
+++ Selenium18Tests/Selenium18Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium18Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium18Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("585f225e-99fb-41b7-bd19-f47c3a2f4e50")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Selenium18Tests/Selenium18Tests/SimpleTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium18Tests
+{
+	[TestFixture]
+	public class SimpleTests
+	{
+		[Test]
+		public void Google_GoToSearchPage_Success()
+		{
+			var url = "https://www.google.ru/search?q=Testing+with+Selenium+is+fun&oq=Testing+with+Selenium+is+fun";
+			var ChromeDriver = new ChromeDriver();
+			ChromeDriver.Navigate().GoToUrl(url);
+			ChromeDriver.Close();
+		}
+	}
+}
