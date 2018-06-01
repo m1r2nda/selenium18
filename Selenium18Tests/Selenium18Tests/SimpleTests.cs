23e51cdcf747eee4915664b559235cb3f8650fec
Tretiakova Marina <m1r2nda@skbkontur.ru>
2018-06-01 18:07:05 +0500

    Task 3. Login test

--- Selenium18Tests/Selenium18Tests/SimpleTests.cs
+++ Selenium18Tests/Selenium18Tests/SimpleTests.cs
@@ -1,18 +1,58 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace Selenium18Tests
 {
 	[TestFixture]
 	public class SimpleTests
 	{
+		private IWebDriver webDriver;
+		private WebDriverWait wait;
+
+		[SetUp]
+		public void SetUp()
+		{
+			webDriver = new ChromeDriver();
+			wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			webDriver.Close();
+			webDriver = null;
+		}
+
 		[Test]
 		public void Google_GoToSearchPage_Success()
 		{
 			var url = "https://www.google.ru/search?q=Testing+with+Selenium+is+fun&oq=Testing+with+Selenium+is+fun";
-			var ChromeDriver = new ChromeDriver();
-			ChromeDriver.Navigate().GoToUrl(url);
-			ChromeDriver.Close();
+			webDriver.Navigate().GoToUrl(url);
+		}
+
+		[Test]
+		public void AdminPage_Login_Success()
+		{
+			//Переходим на страницу логина в админку
+			webDriver.Navigate().GoToUrl("http://localhost:81/litecart/admin/");
+			wait.Until(webDriver => webDriver.Title.Equals("My Store"));
+
+			//Вводим логин-пароль
+			webDriver.FindElement(By.XPath("//input[@name='username']")).SendKeys("admin");
+			webDriver.FindElement(By.XPath("//input[@name='password']")).SendKeys("admin");
+
+			//Нажимаем "Login"
+			webDriver.FindElement(By.XPath("//button[@name='login']")).Click();
+
+			//ASSERT
+			var isSuccessMessageDisplayed = webDriver.FindElement(By.XPath("//*[@class='notice success']")).Displayed;
+			var successMessage = webDriver.FindElement(By.XPath("//*[@class='notice success']")).Text;
+
+			Assert.IsTrue(isSuccessMessageDisplayed, "Не отобразилось сообщение об успешном входе в админку");
+			Assert.AreEqual("You are now logged in as admin", successMessage, "Отличается от ожидаемого приветственный текст при входе в админку");
 		}
 	}
 }
