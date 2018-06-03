9cda7fb43e4b405f14fb2758b6f8f458b2af3df8
Tretiakova Marina <m1r2nda@skbkontur.ru>
2018-06-03 21:14:53 +0500

    Task 7. All left panel menu items test

--- Selenium18Tests/Selenium18Tests/SimpleTests.cs
+++ Selenium18Tests/Selenium18Tests/SimpleTests.cs
@@ -16,6 +16,7 @@ namespace Selenium18Tests
 		public void SetUp()
 		{
 			webDriver = new ChromeDriver();
+
 			wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(10));
 		}
 
@@ -23,6 +24,7 @@ namespace Selenium18Tests
 		public void TearDown()
 		{
 			webDriver.Close();
+			webDriver.Quit();
 			webDriver = null;
 		}
 
@@ -53,6 +55,44 @@ namespace Selenium18Tests
 
 			Assert.IsTrue(isSuccessMessageDisplayed, "Не отобразилось сообщение об успешном входе в админку");
 			Assert.AreEqual("You are now logged in as admin", successMessage, "Отличается от ожидаемого приветственный текст при входе в админку");
+		}
+
+		[Test]
+		public void AdminPage_GoToLeftPanelMenuItem_Success()
+		{
+			//Переходим на страницу логина в админку
+			webDriver.Navigate().GoToUrl("http://localhost:81/litecart/admin/");
+			wait.Until(webDriver => webDriver.Title.Equals("My Store"));
+
+			//Авторизуемся в админке
+			webDriver.FindElement(By.XPath("//input[@name='username']")).SendKeys("admin");
+			webDriver.FindElement(By.XPath("//input[@name='password']")).SendKeys("admin");
+			webDriver.FindElement(By.XPath("//button[@name='login']")).Click();
+
+			//Кликаем на все ссылки в левой панели и проверяем наличие заголовка
+			var leftPanelMenuItemsCount = webDriver.FindElements(By.XPath("//*[@id='app-']")).Count;
+			for (var i=0; i < leftPanelMenuItemsCount; i++)
+			{
+				webDriver.FindElements(By.XPath("//*[@id='app-']"))[i].Click();
+
+				//ASSERT
+				Assert.IsTrue(IsElementPresent(By.TagName("h1")), "Не отобразился заголовок при переходе в меню из левой панели админки");
+
+				//Прокликиваем так же подпункты меню последовательно
+				var leftPanelMenuSubItemsCount = webDriver.FindElements(By.XPath("//*[@id='app-']//li")).Count;
+				for (var j = 0; j < leftPanelMenuSubItemsCount; j++)
+				{
+					webDriver.FindElements(By.XPath("//*[@id='app-']//li"))[j].Click();
+
+					//ASSERT
+					Assert.IsTrue(IsElementPresent(By.TagName("h1")), "Не отобразился заголовок при переходе в меню из левой панели админки");
+				}
+			}
+		}
+
+		private bool IsElementPresent(By locator)
+		{
+			return webDriver.FindElements(locator).Count > 0;
 		}
 	}
 }
