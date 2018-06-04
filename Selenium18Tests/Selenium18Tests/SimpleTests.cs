44ae92eded37caddeb9fcaef7da6a844149dd5d3
Tretiakova Marina <m1r2nda@skbkontur.ru>
2018-06-04 09:55:41 +0500

    Task 10. Campaign product test

--- Selenium18Tests/Selenium18Tests/SimpleTests.cs
+++ Selenium18Tests/Selenium18Tests/SimpleTests.cs
@@ -108,6 +108,56 @@ namespace Selenium18Tests
 		}
 
 		[Test]
+		public void HomePage_GoToCampaignProduct_Success()
+		{
+			//Переходм на главную страницу
+			webDriver.Navigate().GoToUrl("http://localhost:81/litecart");
+			wait.Until(webDriver => webDriver.Title.Equals("Online Store | My Store"));
+
+			//Находим первый товар из раздела Campaigns и запоминаем данные о нем
+			var productNameOnMainPage = webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//*[@class='name']")).Text;
+			var campaignPriceOnMainPage = webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//*[@class='campaign-price']")).Text;
+			var campaignPriceColorOnMainPage = webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//*[@class='campaign-price']")).GetCssValue("color").Replace("rgba(","").Replace(")","").Replace(" ","").Split(',');
+			var campaignPriceTagNameOnMainPage = webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//*[@class='campaign-price']")).TagName;
+			var campaignPriceFontSizeOnMainPage = webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//*[@class='campaign-price']")).Size.Height;
+
+			var regularPriceColorOnMainPage = webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//*[@class='regular-price']")).GetCssValue("color").Replace("rgba(", "").Replace(")", "").Replace(" ","").Split(','); 
+			var regularPriceFontSizeOnMainPage = webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//*[@class='regular-price']")).Size.Height;
+			var regularPriceOnMainPage = webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//*[@class='regular-price']")).Text;
+			var regularPriceTagNameOnMainPage = webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//*[@class='regular-price']")).TagName;
+			
+			//Переходим в товар
+			webDriver.FindElement(By.XPath("(//*[@id='box-campaigns']//li)[1]//a[@class='link']")).Click();
+
+			//Проверяем данные о товаре
+			var productNameOnProductPage = webDriver.FindElement(By.XPath("//*[@id='box-product']//h1[@class='title']")).Text;
+			var campaignPriceTagNameOnProductPage = webDriver.FindElement(By.XPath("//*[@id='box-product']//*[@class='campaign-price']")).TagName;
+			var campaignPriceColorOnProductPage = webDriver.FindElement(By.XPath("//*[@id='box-product']//*[@class='campaign-price']")).GetCssValue("color").Replace("rgba(", "").Replace(")", "").Replace(" ","").Split(',');
+			var campaignPriceOnProductPage = webDriver.FindElement(By.XPath("//*[@id='box-product']//*[@class='campaign-price']")).Text;
+			var campaignPriceFontSizeOnProductPage = webDriver.FindElement(By.XPath("//*[@id='box-product']//*[@class='campaign-price']")).Size.Height;
+
+			var regularPriceOnProductPage = webDriver.FindElement(By.XPath("//*[@id='box-product']//*[@class='regular-price']")).Text;
+			var regularPriceColorOnProductPage = webDriver.FindElement(By.XPath("//*[@id='box-product']//*[@class='regular-price']")).GetCssValue("color").Replace("rgba(", "").Replace(")", "").Replace(" ","").Split(',');
+			var regularPriceFontSizeOnProductPage = webDriver.FindElement(By.XPath("//*[@id='box-product']//*[@class='regular-price']")).Size.Height;
+			var regularPriceTagNameOnProductPage = webDriver.FindElement(By.XPath("//*[@id='box-product']//*[@class='regular-price']")).TagName;
+
+			Assert.AreEqual(productNameOnMainPage, productNameOnProductPage, "Не совпадает название товара на главной и на странице самого товара");
+			Assert.AreEqual(regularPriceOnMainPage, regularPriceOnProductPage, "Не совпадает обычная цена товара на главной и на странице самого товара");
+			Assert.AreEqual(campaignPriceOnMainPage, campaignPriceOnProductPage, "Не совпадает скидочная цена товара на главной и на странице самого товара");
+			Assert.AreEqual("s", regularPriceTagNameOnMainPage, "Не зачеркнута обычная цена товара на главной странице");
+			Assert.AreEqual("s", regularPriceTagNameOnProductPage, "Не зачеркнута обычная цена товара на странице самого товара");
+			Assert.IsTrue(campaignPriceFontSizeOnMainPage - regularPriceFontSizeOnMainPage > 0, "Аукционная цена не больше обычной на главной странице");
+			Assert.IsTrue(campaignPriceFontSizeOnProductPage - regularPriceFontSizeOnProductPage > 0, "Аукционная цена не больше обычной на странице самого товара");
+			Assert.IsTrue(regularPriceColorOnProductPage[0].Equals(regularPriceColorOnProductPage[1]) && regularPriceColorOnProductPage[1].Equals(regularPriceColorOnProductPage[2]), "Обычная цена не серого цвета на странице самого товара");
+			Assert.IsTrue(regularPriceColorOnMainPage[0].Equals(regularPriceColorOnMainPage[1]) && regularPriceColorOnMainPage[1].Equals(regularPriceColorOnMainPage[2]), "Обычная цена не серого цвета на главной странице");
+			Assert.AreEqual("strong", campaignPriceTagNameOnMainPage, "Аукционная цена не жирная на главной странице");
+			Assert.AreEqual("strong", campaignPriceTagNameOnProductPage, "Аукционная цена не жирная на странице самого товара");
+			Assert.IsTrue(campaignPriceColorOnProductPage[1]=="0" && campaignPriceColorOnProductPage[2]=="0" && campaignPriceColorOnProductPage[0]!="0", "Цвет аукционной цены товара не красного цвета на странице самого товара");
+			Assert.IsTrue(campaignPriceColorOnMainPage[1]=="0" && campaignPriceColorOnMainPage[2]=="0" && campaignPriceColorOnMainPage[0]!="0", "Цвет аукционной цены товара не красного цвета на главной странице");
+		}
+
+
+		[Test]
 		public void AdminPage_Countries_AreInAlphabeticalOrder()
 		{
 			//Переходим на страницу логина в админку
@@ -129,7 +179,6 @@ namespace Selenium18Tests
 			{
 				Assert.IsTrue(String.CompareOrdinal(countries[i].Text, countries[i+1].Text) <=0, "В списке стран не соблюдается алфавитный порядок");
 			}
-
 		}
 
 		[Test]
@@ -160,7 +209,6 @@ namespace Selenium18Tests
 					Assert.IsTrue(String.CompareOrdinal(zones[i].Text, zones[i + 1].Text) <= 0, "В списке зон не соблюдается алфавитный порядок");
 				}
 			}
-
 		}
 
 		[Test]
@@ -191,7 +239,6 @@ namespace Selenium18Tests
 					Assert.IsTrue(String.CompareOrdinal(zones[i].Text, zones[i + 1].Text) <= 0, "В списке зон не соблюдается алфавитный порядок");
 				}
 			}
-
 		}
 
 		private bool IsElementPresent(By locator)
